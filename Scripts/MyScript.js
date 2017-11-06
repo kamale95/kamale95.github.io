849d31377969483349fad6459c86dabe510210f8
Kamalesh <kamale.flash@gmail.com>
2017-11-06 20:10:25 +0530

    First WebPage Upload
    Profile Page

---
+++ Scripts/MyScript.js
@@ -0,0 +1,25 @@
+﻿//Scroll Animation Script
+$(document).ready(function () {
+    $('.nav a').click(function () {
+        $('.nav-collapse').collapse('hide');
+    });
+})
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        var target = this.hash;
+        var $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 600, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
+
+//Hide Navbar Menu on Click 
+$(document).on('click', '.navbar-collapse.in', function (e) {
+    if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+        $(this).collapse('hide');
+    }
+});
